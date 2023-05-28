4c42c1b4001bdf8b2e3763e5e41c9378edd8b512
Sanjib Das <sanjibdas86@gmail.com>
2023-05-28 08:31:36 +0530

    Added Claases and interfaces for Base Core project

---
+++ Sanfy.In.Base.Core/BaseDomainEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Sanfy.In.Base.Core.Interfaces;
+
+namespace Sanfy.In.Base.Core
+{
+	public class BaseDomainEvent : IDomainEvent
+    {
+        public Guid Id { get; }
+
+        public DateTime OccurredOn { get; }
+
+        public BaseDomainEvent()
+        {
+            this.Id = Guid.NewGuid();
+            this.OccurredOn = DateTime.UtcNow;
+        }
+    }
+}
---
+++ Sanfy.In.Base.Core/BaseEntity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Sanfy.In.Base.Core.Interfaces;
+
+namespace Sanfy.In.Base.Core
+{
+    public abstract class BaseEntity
+    {
+        private List<IDomainEvent> _domainEvents;
+
+        /// <summary>
+        /// Domain events occurred.
+        /// </summary>
+        public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents?.AsReadOnly();
+
+        public void ClearDomainEvents()
+        {
+            _domainEvents?.Clear();
+        }
+
+        /// <summary>
+        /// Add domain event.
+        /// </summary>
+        /// <param name="domainEvent">Domain event.</param>
+        protected void AddDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents ??= new List<IDomainEvent>();
+
+            this._domainEvents.Add(domainEvent);
+        }
+
+        protected void CheckRule(IBusinessRule rule)
+        {
+            if (rule.IsBroken())
+            {
+                throw new BusinessRuleValidationException(rule);
+            }
+        }
+    }
+}
